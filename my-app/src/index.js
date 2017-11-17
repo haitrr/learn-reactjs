b08530a598ad7f95422787c0479d405364804fda
Hai Van Tran <haitran@2cs.local>
2017-11-17 11:24:27 +0700

    Use two for loops to render game board instead of hard code

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -19,25 +19,18 @@ function Square(props) {
     }
   
     render() {
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+      var rows = [];
+      for(var i =0 ; i<3;i++){
+        var cells = [];
+        for(var j =0;j<3;j++){
+          cells.push(this.renderSquare(i*3+j));
+        }
+        rows.push(<div className="board-row">
+        {cells}
         </div>
-      );
+        )
+      }
+      return rows;
     }
   }
   
